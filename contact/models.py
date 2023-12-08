02fb2521bab8f68795ab3469c5524722efd03a5d
Bruno Migliorini <cyberbex@gmail.com>
2023-12-08 00:45:44 +0000

    aula 462

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -9,6 +10,15 @@ from django.utils import timezone
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
 
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -17,3 +27,15 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    owner = models.ForeignKey(
+        User,on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
