b939c335bcf97142db74058978f1e3020d0a3d5e
Bruno Migliorini <cyberbex@gmail.com>
2023-12-09 05:57:50 +0000

    472-usando a classe Paginator para a paginaçao do django

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
@@ -8,10 +9,14 @@ def index(request):
 
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('id')[0:10]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts':contacts,
+        'page_obj': page_obj,
         'site_title':'Contatos - '
     }
     
@@ -31,15 +36,18 @@ def search(request):
     contacts = Contact.objects \
         .filter(show=True)\
         .filter(
-            Q(first_name__icontains=search_value) 
-            #Q(last_name__icontains=search_value) |
-            #Q(phone__icontains=search_value) |
-            #Q(email__icontains=search_value) 
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value) 
         )\
         .order_by('-id')
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts':contacts,
+        'page_obj':page_obj,
         'site_title':'Search - '
     }
     
