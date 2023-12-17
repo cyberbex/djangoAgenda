87a668b202388ef48bb38ad12d808cae4cf714df
Bruno Migliorini <cyberbex@gmail.com>
2023-12-17 19:07:24 +0000

    aula 484 savando dados do formulário

--- contact/urls.py
+++ contact/urls.py
@@ -5,8 +5,11 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path("<int:contact_id>/", views.contact, name='contact'),
-    path("search/", views.search, name='search'),
-    path("", views.index, name='index'),
     
+    path("", views.index, name='index'),
+    path("search/", views.search, name='search'),
+
+    #contact(CRUD)
+    path("<int:contact_id>/", views.contact, name='contact'),
+    path("contact/create/", views.create, name='create'),    
 ]
